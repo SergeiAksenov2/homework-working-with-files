d388abc46facbf85371de0a71e9e04e973a8092b
Sergei Aksenov <s.aksenov@haulmont.com>
2024-02-06 18:06:09 +0400

    Added: photo типа byte[], 'generated column' with LinkButton, downloading files.

--- src/main/java/com/sample/drivers/screen/driver/DriverEdit.java
+++ src/main/java/com/sample/drivers/screen/driver/DriverEdit.java
@@ -1,10 +1,36 @@
 package com.sample.drivers.screen.driver;
 
+import com.sample.drivers.entity.Document;
+import io.jmix.ui.UiComponents;
+import io.jmix.ui.component.Component;
+import io.jmix.ui.component.LinkButton;
+import io.jmix.ui.download.Downloader;
 import io.jmix.ui.screen.*;
 import com.sample.drivers.entity.Driver;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @UiController("Driver.edit")
 @UiDescriptor("driver-edit.xml")
 @EditedEntityContainer("driverDc")
 public class DriverEdit extends StandardEditor<Driver> {
+
+    @Autowired
+    private UiComponents uiComponents;
+
+    @Autowired
+    private Downloader downloader;
+
+    @Install(to = "documentsTable.attachment", subject = "columnGenerator")
+    private Component documentsTableAttachmentColumnGenerator(final Document document) {
+
+        if (document.getAttachment() != null) {
+            LinkButton linkButton = uiComponents.create(LinkButton.class);
+            linkButton.setCaption(document.getAttachment().getFileName());
+            linkButton.addClickListener(clickEvent -> {
+                downloader.download(document.getAttachment());
+            });
+            return linkButton;
+        }
+        return null;
+    }
 }
